88dc3286ed07fae23237ff39f9deccea307f9966
airat <airat-4@mail.ru>
2016-11-01 21:44:13 +0400

    commit

--- src/task1/formula/SimpleFormula.java
+++ src/task1/formula/SimpleFormula.java
@@ -10,11 +10,55 @@ import java.util.Map;
  */
 public class SimpleFormula  implements Formula{
     private static ArrayList<Operator> operators = new ArrayList<>();
-    private ArrayList formulaItems = new ArrayList();
+    private ArrayList<Object> formulaItems = new ArrayList<>();
 
 
     public SimpleFormula(String formula) {
+        formula = dividedItemsWithSpace(formula);
+        String[] items = formula.split("\\s+");
+        addItems(items);
+        for (String item : items) {
+            if (isSuchOperator(item)) {
+                formulaItems.add(getOperator(item));
+            } else {
+                formulaItems.add(new Operand(item));
+            }
+        }
+    }
 
+    private void addItems(String[] items) {
+        for (String item : items) {
+            if (isSuchOperator(item)) {
+                formulaItems.add(getOperator(item));
+            } else {
+                formulaItems.add(new Operand(item));
+            }
+        }
+    }
+
+    private String dividedItemsWithSpace(String formula) {
+        for (Operator operator : operators) {
+            formula = formula.replace(operator.getStringView(), " " + operator.getStringView() + " ");
+        }
+        return formula;
+    }
+
+    private Operator getOperator(String item) {
+        for (Operator operator : operators) {
+            if(item.equals(operator.getStringView())){
+                return operator;
+            }
+        }
+        return null;
+    }
+
+    private boolean isSuchOperator(String item) {
+        for (Operator operator : operators) {
+            if(item.equals(operator.getStringView())){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
